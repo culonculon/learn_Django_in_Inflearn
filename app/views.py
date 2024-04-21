d4fa1edb0039d75676b38d304dc799866ffc771a
culonculon <culonculon48@gmail.com>
2024-04-22 03:36:05 +0900

    [fix] do black~

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(
+        """
+
+        """
+    )
